16a26aeb9e390324dac5f4036d85472a7e433d10
Byron Doria <brsdoria@gmail.com>
2024-12-21 16:56:41 -0300

    commit realizado

--- src/DevIO.Api/obj/Debug/net6.0/DevIO.Api.AssemblyInfo.cs
+++ src/DevIO.Api/obj/Debug/net6.0/DevIO.Api.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevIO.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7930dca6a236089572fcca32969850c78f9bed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9bc4dc9369b7892a80675473f2701dfb524c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevIO.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevIO.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
